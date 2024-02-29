c9e95bc1e3f7df3d99a90f6ba43812b15e99265a
mohamadayman737@gmail.com <mohamadaymam737@gmail.com>
2024-02-29 21:49:11 +0200

    remove LogController

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace App\Providers;
 
-use App\Http\Controllers\CustomLaravelLog;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
-use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        LogViewer::extend('laravel', CustomLaravelLog::class);
         Paginator::useBootstrap();
     }
 }
